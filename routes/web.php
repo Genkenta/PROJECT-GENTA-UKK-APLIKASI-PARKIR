bf38a03f5caa6197f8425bf44b10c21e8f0cf3c2
Genkenta <gentapriyakencana@gmail.com>
2026-04-05 08:28:58 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,16 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CustomLoginController;
+use App\Http\Controllers\StrukController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/login', [CustomLoginController::class, 'index'])->name('login');
+Route::post('/login', [CustomLoginController::class, 'authenticate'])->name('authenticate');
+
+Route::get('/struk/{id}', [StrukController::class, 'print'])
+    ->name('struk.print');
+
 
 // require __DIR__.'/auth.php';
