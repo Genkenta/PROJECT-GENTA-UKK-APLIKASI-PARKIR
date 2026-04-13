e2704ff2906d503afb52e23ffcbe3974d32af368
Genkenta <gentapriyakencana@gmail.com>
2026-04-13 09:10:45 +0700

    Update TarifParkirForm.php

--- app/Filament/Admin/Resources/TarifParkir/Schemas/TarifParkirForm.php
+++ app/Filament/Admin/Resources/TarifParkir/Schemas/TarifParkirForm.php
@@ -4,6 +4,7 @@ namespace App\Filament\Admin\Resources\TarifParkir\Schemas;
 
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\TextInput;
+use App\Models\TarifParkir;
 use Filament\Forms\Components\Select;
 
 class TarifParkirForm
@@ -13,13 +14,18 @@ class TarifParkirForm
         return $schema
             ->components([
                 Select::make('jenis_kendaraan')
-                ->label('Jenis Kendaraan')
-                    ->options([
-                        'motor'   => 'Motor',
-                        'mobil'   => 'Mobil',
-                        'lainnya' => 'Lainnya',
-                    ])
-                    ->required(),
+                   ->label('Jenis Kendaraan')
+                   ->options([
+                   'motor'   => 'Motor',
+                   'mobil'   => 'Mobil',
+                   'lainnya' => 'Lainnya',
+    ])
+    ->unique(
+        table: TarifParkir::class,
+        column: 'jenis_kendaraan',
+        ignoreRecord: true,
+    )
+    ->required(),
 
                     TextInput::make('tarif_per_jam')
                     ->label('Tarif Per Jam')
